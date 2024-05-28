5d3fe48d0788342c5630a919fbc361ff94b5bf81
NAmirov <NAmirov@elordaeco.kz>
2024-05-28 09:50:58 +0500

    fix complince

--- frontend/src/app/[locale]/compliance/page.jsx
+++ frontend/src/app/[locale]/compliance/page.jsx
@@ -6,20 +6,19 @@ export default async function Compliance() {
   const domain = "http://127.0.0.1:1337";
   const locale = useLocale();
   const data = await fetchAntikor(locale);
-  console.log("ANTIKOR", data.data[0].attributes.title);
   return (
     <section className="mb-auto container py-12">
       <h2 className="text-3xl font-bold text-prime ">
-        {data.data[0].attributes.title}
+        {data.data[0]?.attributes?.title}
       </h2>
       <div className="mt-4">
-        {data.data[0].attributes.document.map((item) => {
+        {data.data[0]?.attributes?.document.map((item) => {
           return (
             <Link
               className="hover:text-prime hover:bg-secondary transition-all table"
-              href={domain + item.item_document.data.attributes.url}
+              href={domain + item?.item_document?.data?.attributes?.url}
             >
-              {item.title}
+              {item?.title}
             </Link>
           );
         })}
