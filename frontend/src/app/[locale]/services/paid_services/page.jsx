1f7d52ecbee0e38bea0c1fcd8340dea93699346a
ElordaEcoSystem <elordaecosystem_it@mail.ru>
2025-06-26 14:33:31 +0500

    structure and price services update

--- frontend/src/app/[locale]/services/paid_services/page.jsx
+++ frontend/src/app/[locale]/services/paid_services/page.jsx
@@ -16,7 +16,8 @@ export default async function PaidServices() {
         { id: 1, equipment: { ru: 'Илососная машина КО-507', kk: 'Илосос машинасы КО-507' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 32657, withVat: 36576 }, winter: { noVat: 35596, withVat: 39867 } },
         { id: 2, equipment: { ru: 'Каналопромывочная машина КО-512', kk: 'Арнаны жуу машинасы КО-512' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 52582, withVat: 58892 }, winter: { noVat: 55521, withVat: 62183 } },
         { id: 3, equipment: { ru: 'Каналопромывочная машина Camel 1200, VOLVO FM 84 F R', kk: 'Арнаны жуу машинасы Camel 1200, VOLVO FM 84 F R' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 100255, withVat: 112286 }, winter: { noVat: 111226, withVat: 124574 } },
-        { id: 4, equipment: { ru: 'Паровая машина ППУА 1600', kk: 'Бу машина ППУА 1600' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 69065, withVat: 77353 }, winter: { noVat: 81760, withVat: 91572 } },
+        { id: 4, equipment: { ru: 'Вакуумно-каналопромывочная машина VOLVO FM 84F R', kk: 'VOLVO FM 84F R вакуумды каналды жуу машинасы' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 126506, withVat: 141687 }, winter: { noVat: 152579, withVat: 170889 } },
+        { id: 24, equipment: { ru: 'Паровая машина ППУА 1600', kk: 'Бу машина ППУА 1600' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 69065, withVat: 77353 }, winter: { noVat: 81760, withVat: 91572 } },
       ]
     },
     {
@@ -32,6 +33,9 @@ export default async function PaidServices() {
         { id: 9, equipment: { ru: 'Насос центробежный производительностью 300 м3/час (без учета ДТ)', kk: 'Ортатепкiш сорғы, өнімділігі 300 м3/сағ (ДО есепке алынбайды)' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 12909, withVat: 14458 }, winter: { noVat: 12909, withVat: 14458 } },
         { id: 10, equipment: { ru: 'Насос центробежный производительностью 625 м3/час', kk: 'Ортатепкiш сорғы, өнімділігі 625 м3/сағ' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 29537, withVat: 33082 }, winter: { noVat: 34096, withVat: 38188 } },
         { id: 11, equipment: { ru: 'Насос центробежный производительностью 625 м3/час (без учета ДТ)', kk: 'Ортатепкiш сорғы, өнімділігі 625 м3/сағ (ДО есепке алынбайды)' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 15648, withVat: 17526 }, winter: { noVat: 15648, withVat: 17526 } },
+        { id: 25, equipment: { ru: 'Дифференцированный тариф на откачку септика для физических лиц', kk: 'Жеке тұлғалар үшін септикті айдауға арналған сараланған тариф' }, unit: { ru: '1 м3', kk: '1 м3' }, summer: { noVat: "", withVat: 1439 }, winter: { noVat: "", withVat: 1733 } },
+        { id: 26, equipment: { ru: 'Дифференцированный тариф на откачку септика для юридических лиц', kk: 'Заңды тұлғалар үшін септикті айдауға арналған сараланған тариф' }, unit: { ru: '1 м3', kk: '1 м3' }, summer: { noVat: "", withVat: 1871 }, winter: { noVat: "", withVat: 2245 } }
+
       ]
     },
     {
@@ -40,7 +44,125 @@ export default async function PaidServices() {
         kk: 'Нөсерлі кәріз желілерін диагностикалау'
       },
       items: [
-        { id: 12, equipment: { ru: 'Обследование трубопровода при помощи телеинспекционного комплекса', kk: 'Телеинспекциялық кешеннің көмегімен құбырды тексеру' }, unit: '1 п.м.', summer: { noVat: 1429, withVat: 1600 }, winter: { noVat: 1429, withVat: 1600 } },
+        { id: 12, equipment: { ru: 'Обследование трубопровода при помощи телеинспекционного комплекса', kk: 'Телеинспекциялық кешеннің көмегімен құбырды тексеру' },unit: { ru: '1 м.п.', kk: '1 қ.м.' }, summer: { noVat: 1429, withVat: 1600 }, winter: { noVat: 1429, withVat: 1600 } },
+      ]
+    },
+    {
+      category: {
+        "ru": "Строительно-монтажные работы по прокладке сетей ливневой канализации",
+        "kk": "Нөсер кәріз желілерін төсеу бойынша құрылыс-монтаж жұмыстары"
+      },
+      items: [
+        {
+          "id": 27,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=160mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=160mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 17069 },
+          "winter": { "noVat": "", "withVat": 17069 }
+        },
+        {
+          "id": 28,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=200mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=200mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 22758 },
+          "winter": { "noVat": "", "withVat": 22758 }
+        },
+        {
+          "id": 29,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=315mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=315mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 35258 },
+          "winter": { "noVat": "", "withVat": 35258 }
+        },
+        {
+          "id": 30,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=400mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=400mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 48505 },
+          "winter": { "noVat": "", "withVat": 48505 }
+        },
+        {
+          "id": 31,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=500mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=500mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 56250 },
+          "winter": { "noVat": "", "withVat": 56250 }
+        },
+        {
+          "id": 32,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=600mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=600mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 62500 },
+          "winter": { "noVat": "", "withVat": 62500 }
+        },
+        {
+          "id": 33,
+          "equipment": {
+            "ru": "ГНБ с применением установки XCMG XZ-230 d=700mm, без учета стоимости материалов",
+            "kk": "XCMG XZ-230 d=700mm қондырғысымен ГНБ, материалдардың құнын есепке алмағанда"
+          },
+          "unit": { "ru": "1 п.м.", "kk": "1 қ.м." },
+          "summer": { "noVat": "", "withVat": 72860 },
+          "winter": { "noVat": "", "withVat": 72860 }
+        },
+        {
+          "id": 34,
+          "equipment": {
+            "ru": "Устранение неисправностей путем стыковой сварки SDR трубы d=180mm, 200mm, 225mm",
+            "kk": "SDR құбырларын (d=180мм, 200мм, 225мм) түйістіріп дәнекерлеу арқылы ақауларды жою"
+          },
+          "unit": { "ru": "1 стык", "kk": "1 түйіс" },
+          "summer": { "noVat": 17623, "withVat": 19738 },
+          "winter": { "noVat": "", "withVat": "" }
+        },
+        {
+          "id": 35,
+          "equipment": {
+            "ru": "Устранение неисправностей путем стыковой сварки SDR трубы d=250mm, 280mm",
+            "kk": "SDR құбырларын (d=250мм, 280мм) түйістіріп дәнекерлеу арқылы ақауларды жою"
+          },
+          "unit": { "ru": "1 стык", "kk": "1 түйіс" },
+          "summer": { "noVat": 21147, "withVat": 23685 },
+          "winter": { "noVat": "", "withVat": "" }
+        },
+        {
+          "id": 36,
+          "equipment": {
+            "ru": "Устранение неисправностей путем стыковой сварки SDR трубы d=315mm, 355mm",
+            "kk": "SDR құбырларын (d=315мм, 355мм) түйістіріп дәнекерлеу арқылы ақауларды жою"
+          },
+          "unit": { "ru": "1 стык", "kk": "1 түйіс" },
+          "summer": { "noVat": 28197, "withVat": 31580 },
+          "winter": { "noVat": "", "withVat": "" }
+        },
+        {
+          "id": 37,
+          "equipment": {
+            "ru": "Устранение неисправностей путем стыковой сварки SDR трубы d=400mm, 450mm",
+            "kk": "SDR құбырларын (d=400мм, 450мм) түйістіріп дәнекерлеу арқылы ақауларды жою"
+          },
+          "unit": { "ru": "1 стык", "kk": "1 түйіс" },
+          "summer": { "noVat": 35246, "withVat": 39475 },
+          "winter": { "noVat": "", "withVat": "" }
+        }
       ]
     },
     {
@@ -84,6 +206,8 @@ export default async function PaidServices() {
         { id: 21, equipment: { ru: 'Автобус', kk: 'Автобус' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 25231, withVat: 28258 }, winter: { noVat: 31108, withVat: 34841 } },
         { id: 22, equipment: { ru: 'Микроавтобус', kk: 'Микроавтобус' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 20958, withVat: 23473 }, winter: { noVat: 26836, withVat: 30056 } },
         { id: 23, equipment: { ru: 'Авто-ремонтная мастерская', kk: 'Авто-жөндеу шеберханасы' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 26809, withVat: 30026 }, winter: { noVat: 32687, withVat: 36609 } },
+        { id: 38, equipment: { ru: 'Комбинированная дорожная машина на базе Камаз 43253-2010-69 (КДМ)', kk: 'Камаз 43253-2010-69 (КДМ)базасындағы аралас жол машинасы' }, unit: { ru: 'маш/час', kk: 'маш/сағ' }, summer: { noVat: 35534, withVat: 39798 }, winter: { noVat: 47568, withVat: 53276 } },
+        
       ]
     }
   ];
@@ -116,19 +240,19 @@ export default async function PaidServices() {
   };
 
   const t = translations[locale] || translations.ru; // Fallback to Russian if locale is invalid
-  console.log("PAY",data.data[0].attributes?.document.lenght )
-  const text = data.data[0].attributes.content
+  // console.log("PAY",data.data[0].attributes?.document.lenght )
+  const text = data?.data[0].attributes.content
   return (
       <div className="container mx-auto py-8">
         <h2 className="text-3xl font-bold text-def_black mb-4">
             {t.title}
         </h2>
         <div  className="mb-4">
-            {text.map((item)=> {return (
+            {text?.map((item)=> {return (
                 <div className="mt-2">{item.children[0].text}</div>
             )})}
         </div>
-       {data.data[0].attributes?.document.length > 0 &&
+       {data?.data[0].attributes?.document.length > 0 &&
          <div className="mb-8">
          <div className="">
            <div className="text-xl font-medium">{t.doc}</div>
@@ -166,9 +290,9 @@ export default async function PaidServices() {
                   </tr>
                 </thead>
                 <tbody>
-                  {service.items.map((item) => (
+                  {service.items.map((item,index) => (
                     <tr key={item.id} className="hover:bg-gray-50">
-                      <td className="py-2 px-4 border-b border-r">{item.id}</td>
+                      <td className="py-2 px-4 border-b border-r">{index+1}</td>
                       <td className="py-2 px-4 border-b border-r">{item.equipment[locale]}</td>
                       <td className="py-2 px-4 border-b border-r">{item.unit[locale]}</td>
                       <td className="py-2 px-4 border-b border-r text-right">{item.summer.noVat.toLocaleString()}</td>
