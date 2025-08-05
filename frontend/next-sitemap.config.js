df6ffa10c580533afd867f6f1e555eb9d034c0bf
ElordaEcoSystem <elordaecosystem_it@mail.ru>
2025-08-05 09:03:42 +0500

    DNS update

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://elordaecosystem.kz', // ← Замени на свой домен
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
